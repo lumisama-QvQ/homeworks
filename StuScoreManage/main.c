b1d22be2f41103860434622676412d1eaf77a137
lumisama-QvQ <2510421067@stu.hfnu.edu.cn>
2025-12-17 12:15:57 +0800

    完善框架

--- StuScoreManage/main.c
+++ StuScoreManage/main.c
@@ -1,18 +1,12 @@
-/*
-待实现：
-1.数组的动态增长
-2.数组的数据回收
-3.数组的数据插入
-4.数组重复管理
-5.数组的内容搜索
-6.内容排序 */
+//注：此程序使用了GNU标准，部分内容仅在Linux下可以编译。
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#include <time.h> //Linux下使用sleep函数
+#include <time.h> //Linux下使用延时函数
 
+//要求。。。好像没什么用了
 #define MAX_STUDENT 30
 #define MAX_COURSE 6
 
@@ -30,13 +24,11 @@ typedef struct {
     bool attend;
     float score;
 } Subject;
-//
 typedef struct {
    unsigned char *name;
    Subject* Subject;
    float total;
 } Student;
-
 
 
 Student* readfile();
@@ -46,14 +38,15 @@ Student* search_by_name();
 Student* search_by_number();
 Student* total_and_average();
 Student* statistic_analysis();
+float* total(Subject *subjects, int subject_number);
 char* sort();
 Student* input_record();
 
 //数组处理
-void add_student(Student **arr, int *count, int *capacity, const unsigned char,
-                float *raw_scores, int raw_score_count);
+void add_student(Student **arr, int *count, int *capacity,const unsigned char *name, Subject *subjects);
 void format();
 void delete();
+
 
 int main(void) {
     printf("1.Input record\n2.Calculate total and average scoreof every course\n3.Calculate total and average score of every student\n4. Sort in descending order by total score of every student\n5. Sort in ascending order by total score of every student\n6. Sort in ascending order by number\n7. Sort in dictionary order by name\n8. Search by number\n9. Search by name\n10.Statistic analysis for every course\n11.List record\n12.Write to a file\n13.Read from file\n0.Exit\nPlease enter your choice:");
@@ -71,7 +64,7 @@ int main(void) {
         if (choice < 0 || choice > 13) {
             printf("Invalid choice. Please enter a number between 0 and 13.\n");
         } else {
-            printf("You selected option %d.\n", choice);
+            printf("You selected option: %d.\n", choice);
         }
         switch (choice) {
             case 0 :{
@@ -112,9 +105,9 @@ Student* input_record() {
     fflush(stdout);
     scanf("%d", &subject_number);
     getchar();
-    //为Subject分配指定数量的内存
     Subject *subjects = (Subject *)malloc(sizeof(Subject) * subject_number);
 
+    //课程录入
     printf("Now please input name of subjects by whitespace.\n");
     printf(">>>");
     fflush(stdout);
@@ -128,6 +121,8 @@ Student* input_record() {
     for (int i = 0;i < subject_number; i++) {
         printf("%s ", subjects[i].name);
     }
+
+
     printf("\n");
     printf("Please input student's name and each subject score follow the order you input by whitespace.");
     for(int student_order = 0;;student_order++) {
@@ -138,8 +133,8 @@ Student* input_record() {
     }
     }
 
-void add_student(Student **arr, int *count, int *capacity, const unsigned char,
-                float *raw_scores, int raw_score_count) {
+void add_student(Student **arr, int *count, int *capacity,const unsigned char *name, Subject *subjects) {
+                    //动态扩容
                     if (*count >= *capacity) {
                         int new_capacity = (*capacity == 0) ? 4 : (*capacity * 2);
                         Student *temp = (Student *)realloc(*arr, new_capacity * sizeof(Student));
@@ -151,4 +146,23 @@ void add_student(Student **arr, int *count, int *capacity, const unsigned char,
                         *capacity = new_capacity;
                     }
                     Student *target = &(*arr)[*count];
+                    target->name = (unsigned char *)malloc(strlen(name) + 1);
+                    if ( target->name == NULL) {
+                        printf("Error: Not enough memory!");
+                    }
+                    strcpy((char *)target->name, name);
+                    target->Subject = subjects;
+
+                    (*count)++;
                 }
+
+float* total(Subject *subjects, int subject_number) {
+    float result = 0.0;
+    for (int i = 0; i < subject_number; i++){
+        if (subjects->attend == false) {
+            i++;
+        }
+        result += subjects->score;
+    }
+    return &result;
+}
