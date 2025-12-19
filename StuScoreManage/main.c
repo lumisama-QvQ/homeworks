a4ba4d2cdd2ab8939f9a2d9e016e50e45ee5f2e1
lumisama-QvQ <2510421067@stu.hfnu.edu.cn>
2025-12-19 15:57:59 +0800

    优化代码规范

--- StuScoreManage/main.c
+++ StuScoreManage/main.c
@@ -1,5 +1,19 @@
-//注：此程序使用了GNU标准，部分内容仅在Linux下可以编译。
-
+//注：此程序使用了GNU标准，部分内容可能仅在Linux下可以编译。
+/*
+(1) 输入每个学生的学号、姓名和各科考试成绩；✔
+(2) 计算每门课程的总分和平均分；
+(3) 计算每个学生的总分和平均分；✔
+(4) 按每个学生的总分由高到低排出名次表；
+(5) 按每个学生的总分由低到高排出名次表；
+(6) 按学号由小到大排出成绩表；
+(7) 按姓名的字典顺序排出成绩表；
+(8) 按学号查询学生排名及其考试成绩；
+(9) 按姓名查询学生排名及其考试成绩；
+(10) 按优秀(90~100分)、良好(80~89分)、中等(70~79分)、及格(60~69分)、不及格(0~59分)5个类别，对每门课程分别统计每个类别的人数以及所占的百分比；
+(11) 输出每个学生的学号、姓名、各科考试成绩，以及每门课程的总分和平均分；
+(12) 将每个学生的记录信息写入文件；
+(13) 从文件中读出每个学生的记录信息并显示。
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -13,46 +27,55 @@
 //定义延时时钟
 struct timespec req;
 
+typedef enum {
+    student_id,
+    student_name,
+} Search_kind;
 typedef struct {
-    int by_total_desc; //按总分降序
-    int by_total_asc;  //按总分升序
-    int by_number_asc; //按学号升序
-    int by_name_dict;  //按姓名字典序
+    float total;
+    float average;
+} Statistics;
+typedef enum {
+    by_total_desc, //按总分降序
+    by_total_asc, //按总分升序
+    by_number_asc, //按学号升序
+    by_name_dict,  //按姓名字典序
 } Sort_Kind;
 //*char->name
 typedef struct {
-    unsigned char *name;
+    char *name;
     bool attend;
     float score;
 } Subject;
 //*name *Subject
 typedef struct {
-    unsigned char *name;
+    char *name;
     Subject* Subject;
-    float total;
+    float average;
+    Statistics *statistics;
 } Student;
 
 
-Student* readfile();
-void writefile();
-Student* list_record();
-Student* search_by_name();
-Student* search_by_number();
-Student* total_and_average();
-char* statistic_analysis();
-float total(Subject *subjects, int subject_number);
-char* sort();
+
+
+
+Student* readfile();//未实现
+void writefile();//未实现
+Student* list_record();//未实现
+Student* search_by_name();//未实现
+Student* search_by_number();//未实现
+char* statistic_analysis();//未实现
+char* sort();//未实现
 Student* input_record();
+void et_point();
 
 //数据处理
-void add_student(Student **arr, int *count, int *capacity,const unsigned char *name, Subject *subjects, float total);
-void format();
-void delete();
+void add_student(Student **arr, int *count, int *capacity,const char *name, Subject subjects, Statistics statistics);
+void format();//未实现
+void delete();//未实现
+void search(Student *student, Search_kind kind,char *context);//未实现
 
 int main(void) {
-    printf("1.Input record\n2.Calculate total and average scoreof every course\n3.Calculate total and average score of every student\n4. Sort in descending order by total score of every student\n5. Sort in ascending order by total score of every student\n6. Sort in ascending order by number\n7. Sort in dictionary order by name\n8. Search by number\n9. Search by name\n10.Statistic analysis for every course\n11.List record\n12.Write to a file\n13.Read from file\n0.Exit\nPlease enter your choice:");
-    fflush(stdout);
-
     int choice = 0;
     int count = 0;
     int capacity = 0;
@@ -61,6 +84,8 @@ int main(void) {
     Student *students = NULL;
 
     for(;;){
+        printf("1.Input record\n2.Calculate total and average scoreof every course\n3.Calculate total and average score of every student\n4. Sort in descending order by total score of every student\n5. Sort in ascending order by total score of every student\n6. Sort in ascending order by number\n7. Sort in dictionary order by name\n8. Search by number\n9. Search by name\n10.Statistic analysis for every course\n11.List record\n12.Write to a file\n13.Read from file\n0.Exit\nPlease enter your choice:");
+        fflush(stdout);
         scanf("%d", &choice);
         if (choice < 0 || choice > 13) {
             printf("Invalid choice. Please enter a number between 0 and 13.\n");
@@ -71,15 +96,13 @@ int main(void) {
             case 0 :{
                 nanosleep(&req,NULL);
                 printf("Exiting");
-                for(int i = 0; i < 3; i++){
-                    fflush(stdout);
-                    nanosleep(&req,NULL);
-                    printf(".");
-                }               
+                et_point();
                 exit(0);
             };
             case 1 :{
                 students = input_record();
+                printf("Finish input");
+                et_point();
             };
         }
     }    
@@ -90,9 +113,8 @@ int main(void) {
 //输入考试科目数量，各名称。
 Student* input_record() {
     int subject_number = 0;
-    bool attend;
-    float total = 0.0;
     int capacity = 0;
+    Statistics statistics;
     //*student->*name
     //*student->*subject->*name
     Student *students = NULL;
@@ -102,8 +124,8 @@ Student* input_record() {
     scanf("%d", &subject_number);
     getchar();
     //char**=>char*(char**[i])=>string
-    unsigned char **subjects_name = NULL;
-    subjects_name = (unsigned char **)malloc(sizeof(unsigned char *) * subject_number);
+    char **subjects_name = NULL;
+    subjects_name = (char **)malloc(sizeof(char *) * subject_number);
     
     //课程录入
     printf("Now please input name of subjects by whitespace.\n");
@@ -127,9 +149,11 @@ Student* input_record() {
     for(int student_count = 0;;student_count++) {
         printf(">>>");
         fflush(stdout);
-        unsigned char *student_name;
+        char *student_name;
+        int attend = 0;
         scanf("%ms", &student_name);
-        if (student_name == "exit") break;
+        //提前返回，此时students可能为NULL
+        if (strcmp(student_name, "exit") == 0) return students;
         Subject *temp = (Subject *)malloc(sizeof(Subject) * subject_number);
         for(int i = 0; i < subject_number; i++){
             scanf("%f", &temp[i].score);
@@ -140,16 +164,18 @@ Student* input_record() {
             }
             else {
                 temp[i].attend = true;
-                total += temp[i].score;
+                statistics.total += temp[i].score;
+                attend += 1;
             }
+            statistics.average = statistics.total / attend;
         }
-        add_student(&students, &student_count, &capacity, student_name, temp, total);
+        add_student(&students, &student_count, &capacity, student_name, *temp, statistics);
     }
 
     return students;
 }
 
-void add_student(Student **arr, int *count, int *capacity,const unsigned char *name, Subject *subjects, float total) {
+void add_student(Student **arr, int *count, int *capacity,const char *name, Subject subjects, Statistics statistics) {
                     //subjects->temp(input_record)(动态)
                     //动态扩容
                     if (*count >= *capacity) {
@@ -165,24 +191,21 @@ void add_student(Student **arr, int *count, int *capacity,const unsigned char *n
                     }
                     //target(动态)->&(*student(input_record))[student_count(input_record)]
                     Student *target = &(*arr)[*count];
-                    target->name = (unsigned char *)malloc(strlen(name) + 1);
+                    target->name = (char *)malloc(strlen(name) + 1);
                     if ( target->name == NULL) {
                         printf("Error: Not enough memory!");
                     }
                     //target=>name(动态）->name
                     strcpy((char *)target->name, name);
-                    target->Subject = subjects;
-                    target->total = total;
+                    target->Subject = &subjects;
+                    target->statistics = &statistics;
                     (*count)++;
                 }
 
-float total(Subject *subjects, int subject_number) {
-    float result = 0.0;
-    for (int i = 0; i < subject_number; i++){
-        if (subjects->attend == false) {
-            i++;
-        }
-        result += subjects->score;
+void et_point() {
+    for(int i = 0; i < 3; i++){
+        fflush(stdout);
+        nanosleep(&req,NULL);
+        printf(".");
     }
-    return result;
 }
